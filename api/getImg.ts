155c1afbae667dd485199bd8e433470549ef06e8
adam <adamchan@0x11.email>
2022-04-19 17:04:54 +0800

    stream

--- api/getImg.ts
+++ api/getImg.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-const { generateQRStream } = require('../utils/qrcode.js')
-var template = require('art-template');
+import { PassThrough } from 'stream';
+import QRCode from 'qrcode'
 
 export default async function getImg(req: VercelRequest, res: VercelResponse) {
     let text = req.query.text;
@@ -8,8 +8,16 @@ export default async function getImg(req: VercelRequest, res: VercelResponse) {
         res.status(400).send({ error: "请传入 text 参数，以生成二维码" })
     }
     try {
-        let stream = await generateQRStream(text)
-        return res.send(stream)
+        const qrStream = new PassThrough();
+        await QRCode.toFileStream(qrStream, text,
+            {
+                type: 'png',
+                width: 300,
+                errorCorrectionLevel: 'H',
+                margin:2,
+            }
+        );
+        qrStream.pipe(res);
     } catch (error) {
         return res.status(500);
     }
